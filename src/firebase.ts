ef6ec4deb70ad85a3d567885b3e4557ca09eb33f
AYENI DANIEL <starlordflash@gmail.com>
2024-05-30 06:58:23 +0100

    removed waste

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,15 +4,7 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBMEsPyoEvMTOgFHTn54ePMCUb5YVWIgxw",
-//   authDomain: "korre-io.firebaseapp.com",
-//   projectId: "korre-io",
-//   storageBucket: "korre-io.appspot.com",
-//   messagingSenderId: "246394206478",
-//   appId: "1:246394206478:web:99eaf5c2a3fae26e587f5f",
-//   measurementId: "G-9FZS2M3GHZ"
-// };
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
