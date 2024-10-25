a22b9c237f3c3b10845e43d74b60baabe98155be
daniekeys <starlordflash@gmail.com>
2024-10-25 11:38:38 +0100

    sent

--- src/pages/Testing.tsx
+++ src/pages/Testing.tsx
@@ -5,54 +5,55 @@ import pic from "../assets/png/pic.png";
 import { CoachDetails } from '../util/types';
 import ReUseModal from '../components/modal/Modal';
 
-const item : CoachDetails = {
-  bio: "shas",
-  profileImage: pic,
-  id: 0,
-  email: 'dano@email.com',
-  firstName: 'daniel',
-  lastName: '',
-  username: null,
-  gender: null,
-  slug: null,
-  description: '',
-  role: '',
-  country: '',
-  socials: [],
-  languages: [],
-  emailVerified: false,
-  accountSetupCompleted: false,
-  lastSignin: '',
-  languageInterests: [],
-  status: '',
-  qualifications: [],
-  introVideo: '',
 
-  createdAt: '',
-  updatedAt: '',
-  costPerSession: { amount: 200,
-  currency: "",
-  sessionType: 4,}
-}
 
 const Testing = () => {
-  const [openModal, setOpenModal] = useState(true);
-  const [open, setOpen] = useState(true);
+const [dateTime, setDateTime] = useState("");
   return (
-    <div>
-      <h2>
-        THIS IS TESTING 
-      </h2>
-      <div className="w-full flex  gap-8 flex-col p-8">
+    <div className='p-4'>
+      <h2>THIS IS TESTING</h2>
 
-      <input type="date" className='w-[200px]' />
-      <input type="time" className='w-[200px]' />
+      <div className='flex flex-col gap-4'>
+        <h1>Select Date and Time</h1>
+        <DateTimeInput onChange={setDateTime} />
+        <p>Selected DateTime: {dateTime}</p>
       </div>
-      <div>
-        {/* <h1>Paystack Payment Integration</h1>
-        <PaystackPayment /> */}
-      </div>
-     
+    </div>
+  );
+}
+
+
+
+function DateTimeInput({ onChange } :any) {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+  const handleDateChange = (e:any) => {
+    setDate(e.target.value);
+    onChange(`${e.target.value}T${time}`);
+  };
+
+  const handleTimeChange = (e:any) => {
+    setTime(e.target.value);
+    onChange(`${date}T${e.target.value}`);
+  };
+
+  return (
+    <div className="gap-4 flex items-center">
+      <label>Date:</label>
+      <input
+        type="date"
+        className="border border-black p-2"
+        value={date}
+        onChange={handleDateChange}
+      />
+      <label>Time:</label>
+      <input
+        type="time"
+        value={time}
+        className="border border-black p-2"
+        onChange={handleTimeChange}
+      />
     </div>
   );
 }
